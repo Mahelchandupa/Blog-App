16c61872af12320926beb9b8ef9b0ea501bc1825
Mahel Chandupa <mahelchandupa1013@gmail.com>
2023-08-20 12:17:05 +0530

    first commit

---
+++ api/routes/auth.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+//REGISTER
+router.post("/register", async (req, res) =>{
+    try{
+      
+      //secure password using bcrypt (npm add bcrpyt)
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(req.body.password,salt)
+    
+      const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPass,
+      });
+      const user = await newUser.save();
+      res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(400).json("Wrong credentials!");
+        }
+      
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        if (!validated) {
+            return res.status(400).json("Wrong credentials!");
+        }
+
+        const {password, ...others} = user._doc; //showing user details without password
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router
